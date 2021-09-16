893f960a680243779ac7ba0afc7ff3de30ddb87f
Alex Grobovsky <ktotaam@mail.ru>
2021-09-16 12:07:35 +0500

    save

---
+++ unrm/02.types/index.ts
@@ -0,0 +1,22 @@
+let str: string;
+
+str = "1";
+
+console.log(str);
+
+// let x = true;
+//
+// x = 2;
+//
+// const y = false;
+//
+// let r = '123';
+//
+// let d = new Date();
+//
+// d = 2;
+
+let p: number = JSON.parse('6');
+let e: string = JSON.parse('6aga');
+
+console.log(p);
