b61d9e7eb339f29f6f32a0165552a379b9b4de40
Alex Grobovsky <ktotaam@mail.ru>
2021-09-21 08:51:47 +0500

    save

--- yakov-fain/01/01.test.ts
+++ yakov-fain/01/01.test.ts
@@ -1,34 +1,35 @@
 class Dog {
-    age;
-
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+    constructor(readonly name: string) {}
 
     sayHello = (): string => {
         return "Woof woof";
     }
 }
 
-console.log(Dog);
 
-// class Fish {
-//     name: string;
-//     dive = (howDeep: number): string => {
-//         return `diving ${howDeep} meters`;
-//     }
-// }
-//
-// type Pet = {
-//
-// }
-//
-// const talkToPet = (pet: Pet):string => {
-//     if (pet is)
-// }
-//
-// talkToPet(Fish);
-// talkToPet(Dog);
-// talkToPet({ car: true });
+class Fish {
+    constructor(readonly name: string) {}
+
+    dive = (howDeep: number): string => {
+        return `diving ${howDeep} meters`;
+    }
+}
+
+type Pet = Dog | Fish;
+
+const talkToPet = (pet: Pet): string | undefined => {
+    if (pet instanceof Dog) {
+        return pet.sayHello();
+    }
+    if (pet instanceof Fish) {
+        return "Fish cannot talk, sorry!";
+    }
+}
+
+const mike = new Dog("Mike");
+const dory = new Fish("Dory");
+
+console.log(talkToPet(mike));
+console.log(talkToPet(dory));
+talkToPet({ car: true });
 
---
+++ yakov-fain/fn.ts
@@ -0,0 +1,31 @@
+type taxesType = number | undefined;
+
+const taxes = (state: string, income: number, dependents: number): taxesType => {
+    switch (state) {
+        case "NY":
+            return income * 0.06 - dependents * 500;
+        case "NJ":
+            return income * 0.05 - dependents * 300;
+        default: return income;
+    }
+}
+
+let tax = taxes("NY", 50000, 4);
+
+// class Person {
+//     name: string;
+// }
+// function getP(p: Person){
+//     if (p instanceof Person){ // ошибка компиляции
+//     }
+// }
+
+class Person {
+    name: String;
+    age?: Number;
+}
+class Customer {
+    name: String;
+}
+const cust: Customer = { name: 'Mary' };
+const pers: Person = { name: 'John' };
