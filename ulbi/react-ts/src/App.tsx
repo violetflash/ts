1e0fe5a6ba3a5e430c63c2c485baf56500ea565f
Alex Grobovsky <ktotaam@mail.ru>
2021-11-11 22:48:04 +0500

    save

--- ulbi/react-ts/src/App.tsx
+++ ulbi/react-ts/src/App.tsx
@@ -4,7 +4,7 @@ import {Card} from "./components";
 function App() {
   return (
     <div className="App">
-        <Card width="300px" height="300px" background="purple">
+        <Card width="300px" height="300px" background="purple" variant="outlined">
             <button>Button</button>
         </Card>
     </div>
--- ulbi/react-ts/src/components/Card/Card.tsx
+++ ulbi/react-ts/src/components/Card/Card.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
+export enum CardVariant {
+    outlined = "outlined",
+    primary = "primary",
+}
+
 interface CardI {
     width: string;
     height: string;
     background?: string;
+    variant?: CardVariant | undefined;
 }
 
-export const Card: React.FC<CardI> = ({width, height, children, background = "gray"}) => {
+export const Card: React.FC<CardI> = (
+    {
+        width,
+        height,
+        children,
+        background = "gray",
+        variant = "primary"
+    }) => {
     return (
-        <div style={{width, height, background}}>
+        <div style={{
+            width,
+            height,
+            background: variant === 'primary' ? background : 'transparent',
+            border: "2px solid gray"
+        }}>
             {children}
         </div>
     )
